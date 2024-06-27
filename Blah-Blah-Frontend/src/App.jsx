3bbfe938cbccd7878a0fbf58ced75ff67c12f3ed
manyagaur <manyagaur25@gmail.com>
2024-06-27 23:07:21 +0530

    Initial commit

---
+++ Blah-Blah-Frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+
+import Login from './components/login/login'
+
+
+function App() {
+  
+
+  return (
+    <>
+    <Login/>
+    </>
+  )
+}
+
+export default App
---
+++ Blah-Blah-Frontend/src/components/login/login.jsx
@@ -0,0 +1,46 @@
+
+import './login.scss';
+
+function Login(){
+
+    return(
+    <>
+    <div className="login">
+        <div className="card">
+            <div className="left">
+                <h1>Welcome to BlahBlah</h1>
+                <p>
+                BlahBlah is 
+                the ultimate social networking hub 
+                that turns every conversation into 
+                a vibrant experience. Whether 
+                you're sharing your latest triumphs, 
+                diving into deep discussions, or 
+                simply catching up on the buzz, 
+                Join us today, and 
+                let's turn your chatter into a 
+                symphony of connections!
+                </p>
+            </div>
+            <div className="right">
+                <h3>Login</h3>
+                <form>
+                    <label>Username </label>
+                    <br></br>
+                    <input type="text">
+                    </input>
+                    <br></br>
+                    <label>Password </label>
+                    <br></br>
+                    <input type="password">
+                    </input>
+                    </form>
+            </div>
+        </div> 
+    </div>
+    </>
+    )
+
+}
+
+export default Login;
