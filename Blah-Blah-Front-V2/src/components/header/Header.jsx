315ffd30841c419df5e5765d008dc0e3aa437605
manyagaur <manyagaur25@gmail.com>
2024-07-22 22:27:07 +0530

    implemented responsiveness

--- Blah-Blah-Front-V2/src/components/header/Header.jsx
+++ Blah-Blah-Front-V2/src/components/header/Header.jsx
@@ -1,55 +1,50 @@
 import React, { useState } from 'react';
 import MenuIcon from '@mui/icons-material/Menu';
 import CloseIcon from '@mui/icons-material/Close';
+import logo from '../../assets/logo.png'
 
 function Header() {
+  const [sidebar, setSidebar] = useState(false);
 
- 
-  const [sidebar, setSidebar] = useState(false)
+  const showSidebar = () => setSidebar(true);
+  const hideSidebar = () => setSidebar(false);
 
-  const showSidebar = () => {
-    setSidebar(true)
-  }
-
-  const hideSidebar = () => {
-    setSidebar(false)
-  }
- 
   return (
     <>
-      <div className="grid grid-cols-12 h-9">
-        <div className="col-span-2 bg-white">
-          <div className="m-2 p-2">
-            Logo
+      <div className="grid grid-cols-12 h-24 bg-white">
+        <div className="col-span-2 flex items-center">
+          <div className="mx-3 mt-1 ">
+          <img src={logo} alt="Logo" className="h-28 w-[180px]" />
           </div>
         </div>
-        <div className="col-span-4 bg-white">
-          <input type="search" className="hidden sm:block rounded-md m-2 p-2 sm:w-[170px] w-0 border border-gray-400" />
+        <div className="col-span-4 hidden sm:flex items-center sm:mb-[27px]">
+          <input type="search" className="hidden sm:block rounded-md  border border-gray-400 w-full" />
         </div>
-        <div className="col-span-6 bg-white">
-          <div className="flex justify-start mt-2 p-2">
-            <div className="mx-4"> <a href='#'>Notifs </a></div>
-            <div className="mx-4"><a href='#'> Messages </a></div>
-            <div className="mx-4"><a href='#'> Friends </a></div>
-            <div className="mx-4"><a href='#'> Explore </a></div>
-            <div className='relative'>
-              <div onClick={showSidebar} className='absolute left-[370px] w-[100%]'><a href='#'><MenuIcon/></a></div>
-            </div>
+        <div className="col-span-10 sm:col-span-6 sm:m-8 flex justify-end sm:justify-start">
+          <div className="hidden sm:flex flex-grow justify-between">
+            <a href='#' className="mx-4">Notifs</a>
+            <a href='#' className="mx-4">Messages</a>
+            <a href='#' className="mx-4">Friends</a>
+            <a href='#' className="mx-4">Explore</a>
           </div>
+          <button onClick={showSidebar} className='sm:hidden'>
+            <MenuIcon />
+          </button>
         </div>
       </div>
       {sidebar && (
-      <div className="sidebar fixed top-0 right-0 shadow-xl h-screen w-[350px] bg-gray-200/[0.6] backdrop-blur-xl z-999">
-        <div className="flex flex-col justify-start">
-          
-        <button  onClick={hideSidebar} className='mr-[300px] mt-2'><a href='#'><CloseIcon/></a></button>
-          <button className="h-12 hover:shadow-lg w-[100%]"> <a href='#'>Notifs </a></button>
-          <button className="h-12 hover:shadow-lg w-[100%]"><a href='#'> Messages </a></button>
-          <button className="h-12 hover:shadow-lg w-[100%]"><a href='#'> Friends </a></button>
-          <button className="h-12 hover:shadow-lg w-[100%]"><a href='#'> Explore </a></button>
+        <div className='fixed inset-0 bg-gray-900 bg-opacity-50 z-50'>
+          <div className="sidebar fixed top-0 right-0 shadow-xl h-screen w-64 bg-gray-200/[0.6] backdrop-blur-xl z-999">
+            <div className="flex flex-col p-4">
+              <button onClick={hideSidebar} className='mb-4'><CloseIcon /></button>
+              <a href='#' className="h-12 hover:shadow-lg w-full mb-2">Notifs</a>
+              <a href='#' className="h-12 hover:shadow-lg w-full mb-2">Messages</a>
+              <a href='#' className="h-12 hover:shadow-lg w-full mb-2">Friends</a>
+              <a href='#' className="h-12 hover:shadow-lg w-full mb-2">Explore</a>
+            </div>
+          </div>
         </div>
-      </div>
-    )}
+      )}
     </>
   );
 }
--- Blah-Blah-Front-V2/src/main.jsx
+++ Blah-Blah-Front-V2/src/main.jsx
@@ -1,16 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import img from './assets/8976.jpg'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+import img from './assets/8976.jpg';
 
- // Make sure to replace with the correct path to your image
-
+// Set background image using JavaScript
 document.body.style.backgroundImage = `url(${img})`;
-document.body.style.backgroundSize = 'cover';
-document.body.style.margin = 0;
-document.body.style.padding = 0;
-
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
