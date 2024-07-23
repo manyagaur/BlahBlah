4a348ca8427942e3da2a32f8135e556994c3629a
manyagaur <manyagaur25@gmail.com>
2024-07-24 00:24:31 +0530

    implemented responsiveness

--- Blah-Blah-Front-V2/src/components/header/Header.jsx
+++ Blah-Blah-Front-V2/src/components/header/Header.jsx
@@ -1,7 +1,11 @@
 import React, { useState } from 'react';
 import MenuIcon from '@mui/icons-material/Menu';
 import CloseIcon from '@mui/icons-material/Close';
-import logo from '../../assets/logo.png'
+import logo from '../../assets/logo.png';
+import PersonIcon from '@mui/icons-material/Person';
+import MessageIcon from '@mui/icons-material/Message';
+import NotificationsIcon from '@mui/icons-material/Notifications';
+import SearchIcon from '@mui/icons-material/Search';
 
 function Header() {
   const [sidebar, setSidebar] = useState(false);
@@ -11,36 +15,65 @@ function Header() {
 
   return (
     <>
-      <div className="grid grid-cols-12 h-24 bg-white">
+      <div className="grid grid-cols-12 h-[75px] bg-white">
         <div className="col-span-2 flex items-center">
-          <div className="mx-3 mt-1 ">
-          <img src={logo} alt="Logo" className="h-28 w-[180px]" />
-          </div>
+          <img src={logo} alt="Logo" className="h-28 w-[180px] mx-3" />
         </div>
         <div className="col-span-4 hidden sm:flex items-center sm:mb-[27px]">
-          <input type="search" className="hidden sm:block rounded-md  border border-gray-400 w-full" />
-        </div>
-        <div className="col-span-10 sm:col-span-6 sm:m-8 flex justify-end sm:justify-start">
-          <div className="hidden sm:flex flex-grow justify-between">
-            <a href='#' className="mx-4">Notifs</a>
-            <a href='#' className="mx-4">Messages</a>
-            <a href='#' className="mx-4">Friends</a>
-            <a href='#' className="mx-4">Explore</a>
+          <div className="relative w-full">
+            <input
+              type="search"
+              className="hidden sm:block pl-10 rounded-md border border-gray-400 w-full"
+              placeholder="Search..."
+            />
+            <button className="absolute left-2 top-0" aria-label="Search">
+              <SearchIcon />
+            </button>
           </div>
-          <button onClick={showSidebar} className='sm:hidden'>
-            <MenuIcon />
-          </button>
+        </div>
+        <div className="col-span-10 sm:col-span-6 m-7 flex justify-start">
+          <div className="hidden sm:flex flex-grow sm:col-span-2">
+            <a href="#" className="mx-4">
+              Explore
+            </a>
+            <a href="#" className="mx-4">
+              Home
+            </a>
+          </div>
+          <div className="col-span-8 sm:col-span-2 flex flex-grow">
+            <a href="#" className="mx-4" aria-label="Profile">
+              <PersonIcon />
+            </a>
+            <a href="#" className="mx-4" aria-label="Messages">
+              <MessageIcon />
+            </a>
+            <a href="#" className="mx-4" aria-label="Notifications">
+              <NotificationsIcon />
+            </a>
+          </div>
+          <div className="sm:hidden col-span-2">
+            <a onClick={showSidebar} className="mx-4 flex justify-end" aria-label="Menu">
+              <MenuIcon />
+            </a>
+          </div>
         </div>
       </div>
       {sidebar && (
-        <div className='fixed inset-0 bg-gray-900 bg-opacity-50 z-50'>
+        <div className="fixed inset-0 bg-gray-900 bg-opacity-50 z-50">
           <div className="sidebar fixed top-0 right-0 shadow-xl h-screen w-64 bg-gray-200/[0.6] backdrop-blur-xl z-999">
             <div className="flex flex-col p-4">
-              <button onClick={hideSidebar} className='mb-4'><CloseIcon /></button>
-              <a href='#' className="h-12 hover:shadow-lg w-full mb-2">Notifs</a>
-              <a href='#' className="h-12 hover:shadow-lg w-full mb-2">Messages</a>
-              <a href='#' className="h-12 hover:shadow-lg w-full mb-2">Friends</a>
-              <a href='#' className="h-12 hover:shadow-lg w-full mb-2">Explore</a>
+              <button onClick={hideSidebar} className="mb-4" aria-label="Close Menu">
+                <CloseIcon />
+              </button>
+              <a href="#" className="h-12 hover:shadow-lg w-full mb-2">
+                Notifications
+              </a>
+              <a href="#" className="h-12 hover:shadow-lg w-full mb-2">
+                Messages
+              </a>
+              <a href="#" className="h-12 hover:shadow-lg w-full mb-2">
+                Friends
+              </a>
             </div>
           </div>
         </div>
